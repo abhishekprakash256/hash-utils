0efa2fb7f019443ecdac513caa6a89905ddaa61a
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-05-25 18:28:10 -0400

    tests failing

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -9,30 +9,38 @@ from redis_helper_kit import *
 
 def hash_test():
         
+        #make the instance of helper function
+        helper_fun = redis_crud_operations.Helper_fun( host_name = "localhost")
+        
+        helper_fun.delete_db("primary_set")
+        helper_fun.delete_db("secondary_set")
+        
         hash_utils.generate_unique_hash("primary_set","secondary_set","localhost",5,10,100)
 
         # Connect to Redis server
         #client = redis.StrictRedis(host='localhost', port=6379, db=0)
-
-        #make the instance of helper function
-        helper_fun = redis_crud_operations.Helper_fun( host_name = "localhost")
 
         # Retrieve and print all values in the set
         values = list(helper_fun.get_all_set_val(set_name = "primary_set"))
 
         pop_val = helper_fun.pop_set_val(set_name = "primary_set")
         print("Popped value:", pop_val)
-    
-        length = helper_fun.get_set_len(set_name = "primary_set")
-        print(length)
-        
+
+
         for val in values:
 
             print("Value in the set:", val)
+
+        
+        values2 = list(helper_fun.get_all_set_val(set_name = "secondary_set"))
+
+        for val in values2:
+
+            print("Value in the set2:", val)
+
+        print("Length of primary set:", helper_fun.get_set_len(set_name = "primary_set"))
+        print("Length of secondary set:", helper_fun.get_set_len(set_name = "secondary_set"))
  
-
-
-
         #helper_fun = redis_crud_operations.Helper_fun("test_hash","test_set","localhost")
 
         #for _ in range(10):
